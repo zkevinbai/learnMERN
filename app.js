4e4d304b24ec3aad3e20ba686e4d7a3a9e77830b
Kevin Bai <lkevin957@gmail.com>
2019-03-09 14:48:21 -0800

    Add debugging server

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => res.send("Hello World"));
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
- 
+const db = require('./config/keys').mongoURI;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
 mongoose
     .connect(db, {useNewUrlParser: true})
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");

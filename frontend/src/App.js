bc1cf9bf1490b37a26c9cb07641d24b12745cc0e
Kevin Bai <lkevin957@gmail.com>
2019-03-10 14:19:32 -0700

    up to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import MainPage from './main/main_page';
+
+const App = () => (
+    <Switch>
+        <AuthRoute exact path="/" component={MainPage} />
+    </Switch>
+);
 
 export default App;
